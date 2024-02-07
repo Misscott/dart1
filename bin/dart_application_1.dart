544ac4ef01366a945cc4ef09cff3771a0b1bdd3e
Pau <misscotters@gmail.com>
2024-02-07 19:03:24 +0100

    hola mundo español

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,5 +1,10 @@
 import 'package:dart_application_1/dart_application_1.dart' as dart_application_1;
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_application_1.calculate()}!');
+    // Obtener la fecha y hora actual
+    DateTime now = DateTime.now();
+    // Formatear la fecha y hora
+    String formattedDateTime = "${now.day}-${now.month}-${now.year} ${now.hour}:${now.minute}:${now.second}";
+    // Imprimir el mensaje con la fecha y hora
+    print('Hola mundo: ${dart_application_1.calculate()}! ($formattedDateTime)');
 }
